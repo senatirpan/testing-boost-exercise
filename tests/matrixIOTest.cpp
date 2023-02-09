e99c58f5cb521bb6e42de1bc8c1c9845f181df75
Sena Tarpan <senatirpan.97@gmail.com>
2023-02-09 01:21:33 +0100

    test2.

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -1,17 +1,18 @@
 #define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
 #include <Eigen/Dense>
+#include <boost/test/unit_test.hpp>
 #include "matrixIO.hpp"
 
 using namespace Eigen;
 
 struct matrixIOFixture {
-  matrixIOFixture() {
+  matrixIOFixture()
+  {
 
     matrix = MatrixXd(3, 3);
     matrix << 0.680375, 0.59688, -0.329554,
-              -0.211234, 0.823295, 0.536459,
-              0.566198, -0.604897, -0.444451;
+        -0.211234, 0.823295, 0.536459,
+        0.566198, -0.604897, -0.444451;
   }
 
   MatrixXd matrix;
@@ -19,19 +20,19 @@ struct matrixIOFixture {
 
 BOOST_FIXTURE_TEST_SUITE(matrixIOTests, matrixIOFixture)
 
-BOOST_AUTO_TEST_CASE(matrixIOTests) {
+BOOST_AUTO_TEST_CASE(matrixIOTests)
+{
 
-    MatrixXd result = matrixIO::openData("../data/m3.csv", 3);
+  MatrixXd result = matrixIO::openData("../data/m3.csv", 3);
 
-    BOOST_TEST(matrix(0,0) == result(0,0));
-    BOOST_TEST(matrix(0,1) == result(0,1));
-    BOOST_TEST(matrix(0,2) == result(0,2));
-    BOOST_TEST(matrix(1,0) == result(1,0));
-    BOOST_TEST(matrix(1,1) == result(1,1));
-    BOOST_TEST(matrix(1,2) == result(1,2));
-    BOOST_TEST(matrix(2,0) == result(2,0));
-    BOOST_TEST(matrix(2,1) == result(2,1));
-    BOOST_TEST(matrix(2,2) == result(2,2));
-
+  BOOST_TEST(matrix(0, 0) == result(0, 0));
+  BOOST_TEST(matrix(0, 1) == result(0, 1));
+  BOOST_TEST(matrix(0, 2) == result(0, 2));
+  BOOST_TEST(matrix(1, 0) == result(1, 0));
+  BOOST_TEST(matrix(1, 1) == result(1, 1));
+  BOOST_TEST(matrix(1, 2) == result(1, 2));
+  BOOST_TEST(matrix(2, 0) == result(2, 0));
+  BOOST_TEST(matrix(2, 1) == result(2, 1));
+  BOOST_TEST(matrix(2, 2) == result(2, 2));
 }
 BOOST_AUTO_TEST_SUITE_END()
